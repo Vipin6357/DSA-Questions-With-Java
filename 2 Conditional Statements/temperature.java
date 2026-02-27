0cb93818c59447887668f7fdef8b1e761c31361a
Vipin Gangwar <Vipingangwar264@gmail.com>
2026-02-27 10:22:57 +0530

    Solved One more question

---
+++ 2 Conditional Statements/temperature.java
@@ -0,0 +1,14 @@
+// Finish the following code so that it prints you have a fever if your temperature
+// is above 100 and otherwise prints you don't have a fever.
+
+public class temperature{
+    public static void main(String[] args){
+        double temperature = 103.5;
+
+    if ( temperature > 100 ){
+        System.out.println("you have a fever");
+    } else {
+        System.out.println("you don't have a fever");
+    }
+    }
+}
