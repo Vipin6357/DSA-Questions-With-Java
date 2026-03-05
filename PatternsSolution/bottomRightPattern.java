57305d9e596dfbf5798bab1a7d592722674e8a23
Vipin Gangwar <vipin63966345557@gmail.com>
2026-03-05 19:08:43 +0530

    add Pattern Solutions

---
+++ PatternsSolution/bottomRightPattern.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class bottomRightPattern{
+
+    public static void bottomRight(int n){
+        for (int row = 1; row <= n; row++){
+            for ( int col = 1; col <= n; col++){
+                if(row + col >= n + 1 ){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rown Number of printing star : ");
+        int n = sc.nextInt();
+        sc.close();
+        bottomRight(n);
+        
+        
+    }
+}
