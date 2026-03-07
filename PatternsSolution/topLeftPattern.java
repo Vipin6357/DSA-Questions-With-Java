1abac2c3d94c2a250c7788e1c8d5d6d724fe57b0
Vipin Gangwar <vipin63966345557@gmail.com>
2026-03-07 15:03:50 +0530

    Solve TopLeft Pattern

---
+++ PatternsSolution/topLeftPattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class topLeftPattern {
+     public static void bottomLeft(int n){
+        for (int row = 1; row <= n; row++){
+            for ( int col = 1; col <= n; col++){
+                if( row + col <= n + 1){
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rown Number of printing star : ");
+        int n = sc.nextInt();
+        sc.close();
+        bottomLeft(n);
+    }
+}
+
