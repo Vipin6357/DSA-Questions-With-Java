411128acd81efb7889032c1745148519cbd7cb47
Vipin Gangwar <vipin63966345557@gmail.com>
2026-03-10 12:19:27 +0530

    V shape Pattern

--- PatternsSolution/V_Pattern.java
+++ PatternsSolution/V_Pattern.java
@@ -1,28 +1,57 @@
 import java.util.Scanner;
 
 public class V_Pattern {
-
-        public static void V_shape(int n){
-            for (int row = 1; row <= n; row++){
-                for ( int col = 1; col <= n; col++){
-                    if( row == col && row + col <= n + 1 ||row + col == n + 1 && row <= col ){
-                        
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
-                    }
-                    
+static void v_Pattern(int n){
+        int total_lines = n ;
+        int outerspaces =   0;
+        int innerspaces = 2 * n - 3;
+        int currentLine = 1;
+        int star = 1;
+        while( currentLine <= total_lines){
+            //print spaces
+            
+                for ( int i = 0; i <= outerspaces; i++){
+                System.out.print(" ");
                 }
-                System.out.println();
+            
+
+
+            //print star
+
+            
+            for( int i = 1; i<= star; i++){
+                System.out.print("*");
+
             }
+            
+
+            //print spaces
+            for(int i = 1; i <= innerspaces; i++){
+                System.out.print(" ");
+            }
+            //print star
+            if( outerspaces != n -1   ){
+                for (int i = 1; i <= star; i++){
+                System.out.print("*");
+            }
+            }
+
+            System.out.println();
+            
+            outerspaces++;
+            innerspaces = innerspaces - 2;
+            
+            currentLine++;
+            
         }
 
-        public static void main(String args[]){
-            try(Scanner sc = new Scanner(System.in)){
-            System.out.print("Enter Rown Number of printing star : ");
-            int n = sc.nextInt();
-            sc.close();
-            V_shape(n);
-        }
-   }
+    }
+    public static void main(String[] args) {
+       try(Scanner sc = new Scanner(System.in)){
+        System.out.print("Enter the number of line: ");
+        int n = sc.nextInt();
+        v_Pattern(n);
+       }
+       
+    }
 }
